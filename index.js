b208bd4cf390c4cf262b6cfbb5a3849168f7821a
yamryu <ryuta.yamaguchi425@gmail.com>
2025-07-01 17:51:35 +0900

    電卓アプリの画面設計

---
+++ index.js
@@ -0,0 +1,32 @@
+//(0の場合)
+// 「0」ボタンを取得
+const zeroButton = document.querySelector('.button.zero');
+// ディスプレイの要素を取得
+const display = document.getElementById('display');
+// クリックイベントを追加
+zeroButton.addEventListener('click', () => {
+    const currentValue = display.value;
+    if(currentValue === '0') {
+        return;
+    } else { 
+        display.value += '0';  // 「0」を追加表示
+    }
+});
+
+//(C)の場合
+const clearButton = document.querySelector('.clear.button');
+clearButton.addEventListener('click', () => {
+    const currentValue = display.value;
+    if(currentValue === '0') {
+        return;
+    } else {
+        display.value = '0';
+    }
+});
+
+//(1)の場合
+const numButton = document.getElementsByClassName('button');
+numButton.addEventListener('click', () => {
+    const currentValue = display.value;
+    display.value = currentValue;
+});
---
+++ practice.js
@@ -0,0 +1,75 @@
+//単純な変数定義とその表示
+// let test = 0;
+// let newTest = 1;
+// console.log(test + newTest);
+
+//weekday配列とweekend配列をweek配列に格納して表示
+// let weekday = [
+//     '月曜日',
+//     '火曜日',
+//     '水曜日',
+//     '木曜日',
+//     '金曜日'
+// ]
+// let weekend = [
+//     '土曜日',
+//     '日曜日'
+// ]
+// let week = [];
+
+// week.push(...weekday);
+// week.push(...weekend);
+// alert(week);
+
+
+//バックフォートを使って、表示
+// let userName = 'tarou';
+// console.log(`Hello ${userName}!`);
+
+
+//関数練習
+//function宣言
+// function sayHello(name) {
+//     console.log('てかさ,' + name);
+// }
+// sayHello('疲れた');
+
+//定数に関数を代入
+// const sayHello = function(name) {
+//     console.log('あ、てかさ' + name + 'ほんとに、、');
+// }
+// sayHello('帰りたい');
+
+
+// //undifind
+// let x;
+// console.log(x);
+
+// //null
+// let x = null;
+// console.log(x);
+
+// func = async() => {
+//     await log(3);
+//     await log(2);
+//     await log(1);
+// };
+
+
+// //非同期処理(3秒後に表示)
+// console.log("1. 開始");
+// setTimeout(() => {
+//   console.log("2. 非同期処理（3秒後）");
+// }, 3000);
+
+// console.log("3. 終了");
+
+
+//メソッド紹介
+// document.getElementById();
+// document.getElementsByClassName();
+// const btn1 = document.getElementById('btn1');
+// btn1.addEventListener('dblclick',function() {
+//   display.value = 'ダブルクリックされたよ';
+// });
+
